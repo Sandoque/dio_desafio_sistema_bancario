8f0a50aad91696601f09bcd2ed9b6dddb704de7e
Antonio Carlos Sandoque <acsandoque@gmail.com>
2023-07-15 20:28:30 -0300

    finalizando

--- desafio.py
+++ desafio.py
@@ -25,14 +25,36 @@ while True:
             print("Operação falhou! valor inválidoo.")
 
     elif opcao == "s":
-        print("Saque")
+        valor = float(input("Digite o valor do saque: "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= limites_saques
+
+        if excedeu_saldo:
+            print("Operação falhou! Saldo insuficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! Valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! valor inválidoo.")
 
     elif opcao == "e":
-        print("Extrato")
+        print("\n######################### EXTRATO #########################")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\n Saldo: {saldo:.2f} ")
+        print("===============================================================")
 
     elif opcao == "q":
         break
 
     else:
         print("Operação inválida, favor inserir novamente a operação desejada")
-
